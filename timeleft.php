1d6ee13cd23eaefbc9ffa0f0ffe102366d813513
Grant Bowman <grantbow@gmail.com>
2011-09-04 23:05:41 -0700

    a tiny bit less of a hack

--- timeleft.php
+++ timeleft.php
@@ -2,5 +2,7 @@
 $today = getdate();
 $event = 1315314000;
 printf( "%03.2f hours",  ($event - $today[0]) / 3600 ) ;
+print( "<p><a href=\"http://www.grantbow.com/dreamfish.html\">Thanks for your interest.</a></p> " ) ;
 ?>
+
 
