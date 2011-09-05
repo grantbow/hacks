7a3fb726145c590ebb10383d804d746612e8e3b0
Grant Bowman <grantbow@gmail.com>
2011-09-05 14:05:04 -0700

    added months, fewer decimals.

--- timeleft2.php
+++ timeleft2.php
@@ -2,8 +2,9 @@
 $today = getdate();
 $event = 1323077400;
 printf( "<p>%03.2f hours </p> ", (($event - $today[0]) / 3600 )) ;
-printf( "<p>%03.2f days </p> ", (($event - $today[0]) / 86400 ) ) ;
-printf( "<p>%03.2f weeks </p> ", (($event - $today[0]) / 604800 ) ) ;
+printf( "<p>%03.1f days </p> ", (($event - $today[0]) / 86400 ) ) ;
+printf( "<p>%03.1f weeks </p> ", (($event - $today[0]) / 604800 ) ) ;
+printf( "<p>%02.1f months </p> ", (($event - $today[0]) / 2419200 ) ) ;
 print( "<p><a href=\"http://www.grantbow.com/dreamfish.html\">Thanks for your interest.</a></p> " ) ;
 ?>
 
